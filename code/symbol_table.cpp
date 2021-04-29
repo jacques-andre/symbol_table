485fdb2081c58e8fe123cf0e3f310e2d3e267069
jacques-andre <jacquesandre@mailbox.org>
2021-04-29 18:14:05 +0100

    working on fixing refrences

--- code/symbol_table.cpp
+++ code/symbol_table.cpp
@@ -5,13 +5,12 @@
 #include <vector>
 
 struct data_info{
-    std::string value;
-    int variable;
-    int func;
-    int count;
-    int line_count;
+    std::string value_name;
+    int line_number;
+    int refrence;
+    std::string general_type = "";
+    std::string data_type = "";
 };
-
 int main(int argc, char **argv) {
     // handle input error
     if(argc < 2){
@@ -23,7 +22,7 @@ int main(int argc, char **argv) {
     std::ifstream file(argv[1]);
     std::string line;
     std::string token;
-    std::vector<data_info> dataMappings;
+    std::vector<data_info> data_mappings;
 
     // counters
     int line_counter = 0;
@@ -41,57 +40,71 @@ int main(int argc, char **argv) {
 
         while(stream >> token) {
             if(token == "int" || token == "int*" || token == "long" || token == "long*" || token == "double" || token == "double*" || token == "char" || token == "char*" || token == "short" || token == "*short" || token == "float" || token == "*float") {
+
+                bool found = false; // have not seen the first token
+
                 //We have an int identifier (variable or function)
-                //The next token is the name
-                stream >> token; //Gets the next token (the identifier name)
+                std::string data_type = token;
+                stream >> token; // Gets the next token (the identifier name)
                 std::string name = token;
-                std::cout << "The name of the identifier is: " << name << std::endl;
-                std::cout << "The line number it is declared on is " << line_counter << std::endl;
-                //If the next token is a (, we have a function, else we have
-                //a variable
-                stream >> token; //Get the next token
-                if(token == "(")
-                {
-                    //Have a function
-                    num_functions++;
+
+                // the problem is here, it is not updating the refrence***
+                for(data_info d : data_mappings){
+                    if(d.value_name == name){
+                        std::cout << "am here!" << std::endl;
+                        d.refrence++;
+                        found = true;
+                        break;
+                    }
+                }
+
+                //If the next token is a (, we have a function, else we have a variable
+                stream >> token;
+
+                if(token == "("){
+                    num_functions++; // we have a function
+
+                    // updates the vector
+                    if(!found){
+                        data_info new_data;
+                        new_data.value_name = name;
+                        new_data.line_number = line_counter;
+                        new_data.refrence = 1; // first time we have seen the function
+                        new_data.general_type = "function";
+                        new_data.data_type = data_type;
+                        data_mappings.push_back(new_data);
+                    } 
                 }
                 else {
-                    //Have a variable
-                    num_variables++;
+                    num_variables++; // we have a variable
+
+                    // updates the vector
+                    if(!found){
+                        data_info new_data;
+                        new_data.value_name = name;
+                        new_data.line_number = line_counter;
+                        new_data.refrence = 1; // first time we have seen the function
+                        new_data.general_type = "variable";
+                        new_data.data_type = data_type;
+                        data_mappings.push_back(new_data);
+                    } 
                 }
             }
             else if(token == "if") {
-                stream >> token;
                 num_ifs++;
             }
             else if(token == "while") {
-                stream >> token;
                 num_while++;
             }
             else if(token == "for") {
-                stream >> token;
                 num_for++;
-            }
-            bool found = false;
-            for(auto &w : dataMappings){
-                if(w.value == token){
-                    w.count++;
-                    found = true;
-                    break;
-                }
-            }
-            if(!found){
-                data_info new_data;
-                new_data.value = token;
-                new_data.line_count = line_counter - 1;
-                new_data.count = 1;
-                dataMappings.push_back(new_data);
             }
         }
     }
     file.close();
-    for(auto &w : dataMappings){
-        std::cout << w.value << "\t\t\t" << w.count << "\t\t\t" << w.line_count << std::endl;
+    for(auto &w : data_mappings){
+        std::cout << "\n";
+        std::cout << w.value_name << "," << "line " << w.line_number << "," << w.general_type << "," << w.data_type << "," << "refrenced:" << w.refrence << std::endl;  
     }
     std::cout << "Variables: " << num_variables << std::endl;
     std::cout << "Functions: " << num_functions << std::endl;
