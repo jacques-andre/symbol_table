5ab8f50a84f119414890b73a1eea4069b47967f7
jacques-andre <jacquesandre@mailbox.org>
2021-04-29 16:54:01 +0100

    got the refrences and line, just need to figure out how to know type

---
+++ code/run.sh
@@ -0,0 +1,1 @@
+g++ symbol_table.cpp -o symbol_table && ./symbol_table test-files/test3.c
--- code/symbol_table.cpp
+++ code/symbol_table.cpp
@@ -2,16 +2,28 @@
 #include <string>
 #include <fstream>
 #include <sstream>
+#include <vector>
+
+struct data_info{
+    std::string value;
+    int variable;
+    int func;
+    int count;
+    int line_count;
+};
 
 int main(int argc, char **argv) {
+    // handle input error
     if(argc < 2){
         std::cout << "Please specify an input file!" << std::endl;
         return 1; 
     }
     std::cout << "Reading in the file: " << argv[1] << std::endl;
+
     std::ifstream file(argv[1]);
     std::string line;
     std::string token;
+    std::vector<data_info> dataMappings;
 
     // counters
     int line_counter = 0;
@@ -21,7 +33,8 @@ int main(int argc, char **argv) {
     int num_for = 0;
     int num_while = 0;
 
-    while(getline(file, line)) {
+
+    while(getline(file, line) != NULL) {
         std::stringstream stream(line);
 
         line_counter++;
@@ -32,20 +45,19 @@ int main(int argc, char **argv) {
                 //The next token is the name
                 stream >> token; //Gets the next token (the identifier name)
                 std::string name = token;
-                /* cout << "The name of the identifier is: " << name << endl; */
-                /* cout << "The line number it is declared on is " << line_counter << endl; */
+                std::cout << "The name of the identifier is: " << name << std::endl;
+                std::cout << "The line number it is declared on is " << line_counter << std::endl;
                 //If the next token is a (, we have a function, else we have
                 //a variable
                 stream >> token; //Get the next token
                 if(token == "(")
                 {
-                    //We have a function
+                    //Have a function
                     num_functions++;
                 }
                 else {
-                    //We have a variable
+                    //Have a variable
                     num_variables++;
-
                 }
             }
             else if(token == "if") {
@@ -60,9 +72,27 @@ int main(int argc, char **argv) {
                 stream >> token;
                 num_for++;
             }
+            bool found = false;
+            for(auto &w : dataMappings){
+                if(w.value == token){
+                    w.count++;
+                    found = true;
+                    break;
+                }
+            }
+            if(!found){
+                data_info new_data;
+                new_data.value = token;
+                new_data.line_count = line_counter - 1;
+                new_data.count = 1;
+                dataMappings.push_back(new_data);
+            }
         }
     }
     file.close();
+    for(auto &w : dataMappings){
+        std::cout << w.value << "\t\t\t" << w.count << "\t\t\t" << w.line_count << std::endl;
+    }
     std::cout << "Variables: " << num_variables << std::endl;
     std::cout << "Functions: " << num_functions << std::endl;
     std::cout << "If statements: " << num_ifs << std::endl;
